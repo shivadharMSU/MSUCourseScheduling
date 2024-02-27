4b189b44164eb4ef8605fc8b049160d29988464c
shivadharMSU <shivadharreddy1995@gmail.com>
2024-02-26 21:25:27 -0500

    test

--- src/main/java/com/msu/MsuCourseSchedulingApplication.java
+++ src/main/java/com/msu/MsuCourseSchedulingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsuCourseSchedulingApplication {
 
 	public static void main(String[] args) {
+		///test
 		SpringApplication.run(MsuCourseSchedulingApplication.class, args);
 	}
 
