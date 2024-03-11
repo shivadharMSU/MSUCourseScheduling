8b21bb77491817de6f599e07f77b8f84bf0469d5
shivadharMSU <shivadharreddy1995@gmail.com>
2024-03-10 22:52:31 -0400

    ghbj

--- src/main/java/com/msu/repositories/ClassRoomRepository.java
+++ src/main/java/com/msu/repositories/ClassRoomRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.msu.entities.ClassRoom;
 
 public interface ClassRoomRepository extends JpaRepository<ClassRoom, Integer>{
-
+//test
 }
