980aa097b7d627e4f21ed0cef6d17317887427e8
srinith <srinithsriramula100@gmail.com>
2024-04-16 15:46:24 -0400

    SaveOrUpdateProfessorRequestDTO

--- src/main/java/com/msu/controller/ProfessorController.java
+++ src/main/java/com/msu/controller/ProfessorController.java
@@ -3,11 +3,17 @@ package com.msu.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.msu.DTO.CreateNewSemesterRequestDTO;
+import com.msu.DTO.SaveOrUpdateProfessorRequestDTO;
 import com.msu.DTO.getProfessorResponseDTO;
 import com.msu.DTO.getProfessorTypeResponseDTO;
 import com.msu.services.ProfessorService;
@@ -18,26 +24,13 @@ public class ProfessorController {
 	@Autowired
 	public ProfessorService professorService;
 
-	// This is just a mock list of products, you'd fetch them from a database or
-	// another service in a real application
-//    private List<Products> productList = new ArrayList<>();
-//
-//   
-//
-//    @GetMapping("/products")
-//    public String showProducts(Model model) {
-//    	
-//        model.addAttribute("products", productList);
-//        return "products";
-//    }
-
 	@GetMapping("/getProfessors")
 	@ResponseBody
 	public List<getProfessorResponseDTO> getProfessorList(Model model) {
 
 		return professorService.findAllProfessorsWithDetails();
 	}
-	
+
 	@GetMapping("/getProfessorType")
 	@ResponseBody
 	public List<getProfessorTypeResponseDTO> getProfessorTypesList(Model model) {
@@ -45,15 +38,17 @@ public class ProfessorController {
 		return professorService.findAllProfessorType();
 	}
 	
+	@PostMapping("/saveProfessor")
+	public ResponseEntity<String> saveOrUpdateProfessor(@RequestBody SaveOrUpdateProfessorRequestDTO saveOrUpdateProfessorRequestDTO) {
+		try {
+			professorService.saveOrUpdateProfessor(saveOrUpdateProfessorRequestDTO);
+			return ResponseEntity.ok("success"); // HTTP 200 OK for success
 
-	
+		} catch (Exception ex) {
+			System.out.println("Exception in saveOrUpdateProfessor controller: " + ex.getMessage());
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null); // HTTP 500 Internal Server Error
+																						// for failure
+		}
+	}
 
-
-	/*
-	 * @GetMapping("/create") public String showCreateForm(Model model) {
-	 * model.addAttribute("professor", new Professor()); return "createProfessor"; }
-	 * 
-	 * @PostMapping("/save") public String saveProfessor(Professor professor) {
-	 * professorService.save(professor); return "/create"; }
-	 */
 }
