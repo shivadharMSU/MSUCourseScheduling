c0a9e163e4d9ac02601088e8b9ba035634eb838a
srinith <srinithsriramula100@gmail.com>
2024-11-27 17:49:21 -0500

    classroom service

--- src/main/java/com/msu/services/ClassRomService.java
+++ src/main/java/com/msu/services/ClassRomService.java
@@ -2,13 +2,19 @@ package com.msu.services;
 
 import java.util.List;
 
+import com.msu.DTO.ClassroomReportResponseDTO;
 import com.msu.entities.ClassRoom;
 
 public interface ClassRomService {
-	
+
 	public List<ClassRoom> findAll();
-    public void saveClassRoom(ClassRoom classRoom);
-    public ClassRoom findByRoomId(Integer roomId);
-    public ClassRoom findByRoomName(String roomName);
+
+	public void saveClassRoom(ClassRoom classRoom);
+
+	public ClassRoom findByRoomId(Integer roomId);
+
+	public ClassRoom findByRoomName(String roomName);
+
+	public ClassroomReportResponseDTO getClassroomReportResponse(int semesterId);
 
 }
